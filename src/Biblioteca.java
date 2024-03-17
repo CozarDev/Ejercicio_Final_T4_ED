f2b5aed44b897fd7a8bd1792bd154985d39712da
CozarDev <pedrocozarortiz@gmail.com>
2024-03-17 20:49:28 +0100

    Encapsulación de campos en ambas clases y realización de patrón de refactorización "encapsular arrays"

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,10 +1,28 @@
 public class Biblioteca {
-    public Pelicula[] peliculas;
-
+    private Pelicula[] peliculas;
     public Biblioteca(int capacidad) {
         this.peliculas = new Pelicula[capacidad];
     }
-
+    private void setPeliculas(Pelicula[] var) {
+        this.peliculas = var;
+    }
+    private void setPelicula(int donde, Pelicula pelicula) {
+        this.peliculas[donde] = pelicula;
+    }
+    private Pelicula[] getPeliculas() {
+        return peliculas;
+    }
+    public void setPeliculasCopia (Pelicula[] var) {
+        this.peliculas = new Pelicula[var.length];
+        for (int i = 0; i < var.length; i++) {
+            setPelicula(i, var[i]);
+        }
+    }
+    public Pelicula[] getPeliculasCopia() {
+        Pelicula[] var = new Pelicula[this.peliculas.length];
+        System.arraycopy(this.peliculas, 0, var, 0, this.peliculas.length);
+        return var;
+    }
     public boolean buscadorPorNombre(String titulo) {
         for (Pelicula pelicula : peliculas) {
             if (pelicula.getNombre().equals(titulo)) {
--- src/Pelicula.java
+++ src/Pelicula.java
@@ -4,6 +4,41 @@ public class Pelicula {
     private int duracion;
     private int anio;
 
+    public Pelicula(String nombre, String director, int duracion, int anio) {
+        this.nombre = nombre;
+        this.director = director;
+        this.duracion = duracion;
+        this.anio = anio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
     public String getNombre() {
         return nombre;
     }
