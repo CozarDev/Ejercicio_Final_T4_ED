c4b7d409fb50f74e567f33ebcbc21293c77c3ba6
CozarDev <pedrocozarortiz@gmail.com>
2024-03-17 19:55:17 +0100

    Creación repositorio Ejercicio Final T4 ED

---
+++ src/Biblioteca.java
@@ -0,0 +1,7 @@
+public class Biblioteca {
+    public Pelicula[] peliculas;
+
+    public Biblioteca(int capacidad) {
+        this.peliculas = new Pelicula[capacidad];
+    }
+}
---
+++ src/Pelicula.java
@@ -0,0 +1,6 @@
+public class Pelicula {
+    private String nombre;
+    private String director;
+    private int duracion;
+    private int anio;
+}
